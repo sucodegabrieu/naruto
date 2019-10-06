1e55739eebc26167501f8d2ca31dcf67ed7ab520
sucodegabrieu <54692464+sucodegabrieu@users.noreply.github.com>
2019-10-06 17:43:28 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,11 +2,11 @@ import random, os, sys
 import tweepy
  
 
-arquivos = os.listdir('arquivos')
+pastas = os.listdir('pastas')
 
-numero = random.randrange(0, len(arquivos))
+numero = random.randrange(0, len(pastas))
                    
-foto = arquivos[numero]
+foto = pastas[numero]
 
 consumer_secret = "7Vjq7JwIXK9CC8E2voKersTAUg9PPqf6HegTQsDPKpD561R3on"
 consumer_key = "rcocZOCcbmAQKfsi4tPkitjqY"
@@ -17,7 +17,7 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-file = open('arquivos/' + str(foto), 'rb') 
+file = open('pastas/' + str(foto), 'rb') 
 r1 = api.media_upload(filename='test.png', file=file) 
 
 media = [r1.media_id_string]    
