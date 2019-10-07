e5b0e4c8c48a5d83ee9fc13b993e503b6bc75087
sucodegabrieu <54692464+sucodegabrieu@users.noreply.github.com>
2019-10-06 21:51:56 -0300

    Update server.py

--- server.py
+++ server.py
@@ -2,4 +2,4 @@ from os import environ
 from flask import Flask
 
 app = Flask(__name__)
-app.run(host= '170.0.59.189', port=environ.get('PORT'))
+app.run(host= 'https://terterert.herokuapp.com', port=environ.get('PORT'))
